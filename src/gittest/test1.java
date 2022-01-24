2b269b6134233568c2367a61945d9efb91e6a3a7
onkar <onkar@LAPTOP-239VCABL>
2022-01-24 12:04:44 +0530

    first commit

---
+++ src/gittest/test1.java
@@ -0,0 +1,13 @@
+package gittest;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a =20;
+int b = 10;
+System.out.println(a*b);
+	}
+
+}
+//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
